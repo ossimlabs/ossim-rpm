2526ff2830850d28c229f71a70468a21090d7bc6
Garrett Potts <gpotts@radiantblue.com>
2018-01-08 13:07:25 -0500

    Added script dir for search path

--- scripts/rpmbuild-binary.sh
+++ scripts/rpmbuild-binary.sh
@@ -3,8 +3,10 @@
 GIT_BRANCH="dev"
 OSSIM_SPEC=`uname -r | grep -o el[0-9]`
 ############################################################
-pushd `dirname $0` >/dev/null
+pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
 SCRIPT_DIR=`pwd -P`
+
+export PATH=$SCRIPT_DIR:$PATH
 popd > /dev/null
 
 if [ -z $WORKSPACE ] ; then
